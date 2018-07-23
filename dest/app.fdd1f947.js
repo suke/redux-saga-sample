4919d6082bac0adaf21f4d3c8a843ee67867bee6
suke <suke083.tech@gmail.com>
2018-07-23 23:28:41 +0900

    redux-loggerを追加

--- dest/app.fdd1f947.js
+++ dest/app.fdd1f947.js
@@ -19819,7 +19819,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57198' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57546' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
